25098a0f6b7ba2660a384e038ca5d0aaad9c6bdc
158673770 <44691163+158673770@users.noreply.github.com>
2023-06-08 15:18:59 +0800

    vue2模板v03

---
+++ vue2demoV03/demo1/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Demo  />
+  </div>
+</template>
+
+<script>
+// import Demo from  "./components/contactPerson/index.vue"
+// import Demo from  "./components/video/device.vue"
+import Demo from  "./components/map/map-route.vue"
+
+export default {
+  name: 'App',
+  components: {
+    Demo
+  },
+  data(){
+    return {
+    }
+  },
+  methods:{
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
